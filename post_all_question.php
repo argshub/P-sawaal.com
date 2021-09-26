5e908ebac6893bfd417a0de460c3f7466da4e6d0
pefinite <argshub@outlook.com>
2021-09-26 08:31:50 +0600

    26-09-2021

--- post_all_question.php
+++ post_all_question.php
@@ -4,18 +4,20 @@
 $mysql = new mysqli("127.0.0.1", "root", "", "mysql");
 $mysql->query("create database if not exists sawaal character set utf8 collate utf8_general_ci");
 $mysql = new mysqli("127.0.0.1", "root", "", "sawaal");
-$mysql->query("create table if not exists sawaal_questions (id int auto_increment primary key, questionLabel text unique, questionTitle longtext,questionTags text,questionExplanation longtext )");
+$mysql->query("create table if not exists sawaal_questions (id int auto_increment primary key, questionLabel text unique, questionUri text, questionTitle longtext,questionTags text,questionExplanation longtext )");
 $mysql->query("create table if not exists sawaal_questions_answers (id int auto_increment primary key, questionId int, answer longtext,isCorrect int default 0)");
 
 
 
 $data = json_decode(file_get_contents("php://input"), true);
+
 $questionLabel = $mysql->real_escape_string($data['questionLabel']);
 $questionTitle = $mysql->real_escape_string($data['questionTitle']);
 $questionTags = $mysql->real_escape_string($data['questionTags']);
+$questionUri = $mysql->real_escape_string($data['questionUri']);
 $questionExplanation = $mysql->real_escape_string($data['questionExplanation']);
 $questionOptions = $data['options'];
-$mysql->query("insert into sawaal_questions (questionLabel, questionTitle, questionTags, questionExplanation) value ('{$questionLabel}', '{$questionTitle}', '{$questionTags}', '{$questionExplanation}')");
+$mysql->query("insert into sawaal_questions (questionLabel, questionTitle, questionUri, questionTags, questionExplanation) value ('{$questionLabel}', '{$questionTitle}', '{$questionUri}', '{$questionTags}', '{$questionExplanation}')");
 
 $questionId = $mysql->insert_id;
 if($questionId) {
@@ -44,5 +46,5 @@ if($questionId) {
         }
     }
 }
-
+print_r($questionId);
 $mysql->close();
